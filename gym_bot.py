d9111bc8a3d2b8f9b120313501f473b45ab67c2a
Asaf <AsafsEmail@gmail.com>
2018-03-17 20:39:45 +0200

    switched to MongoDB using Mongoengine

--- gym_bot.py
+++ gym_bot.py
@@ -10,8 +10,8 @@ from datetime import datetime, time, timedelta
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, Filters, MessageHandler
 
-from models_sqlalchemy import User, Group, Day
-from utils import get_db_session, upper_first_letter, WEIGHT_LIFTER_EMOJI, DAYS_NAME, THUMBS_DOWN_EMOJI, THUMBS_UP_EMOJI
+from models import Group, Trainee
+from utils import upper_first_letter, WEIGHT_LIFTER_EMOJI, THUMBS_DOWN_EMOJI, THUMBS_UP_EMOJI
 
 logging.basicConfig(filename='logs/gymbot.log',
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -22,88 +22,81 @@ class GymBot(object):
     REMINDER_TIME = time(hour=9, minute=0, second=0)
     CHECK_WHETHER_DONE_TIME = time(hour=21, minute=0, second=0)
 
-    def __init__(self, db_session, updater, dispatcher, logger):
+    def __init__(self, updater, dispatcher, logger):
         self.updater = updater
         self.dispatcher = dispatcher
-        self.session = db_session
-        self.users = self.session.query(User)
-        self.groups = self.session.query(Group)
-        self.days = self.session.query(Day)
         self.logger = logger
 
-    def _generate_inline_keyboard_for_select_days(self, user):
-        self.logger.info('generation inline keyboard for select days for user %s', user)
-        already_selected_days = user.days
+    def _generate_inline_keyboard_for_select_days(self, trainee):
+        self.logger.info('generation inline keyboard for select days for trainee %s', trainee)
 
         keyboard = []
-        for idx, day in enumerate(self.days.all()):
-            day_name = upper_first_letter(day.name)
-            if day in already_selected_days:
+        for idx, day in enumerate(trainee.training_days):
+            day_name = day.name
+            if day.selected:
                 day_name += ' ' + WEIGHT_LIFTER_EMOJI
-            keyboard.append([InlineKeyboardButton(day_name, callback_data='select_days {id} {idx}'.format(id=user.id,
+
+            keyboard.append([InlineKeyboardButton(day_name, callback_data='select_days {id} {idx}'.format(id=trainee.id,
                                                                                                           idx=idx))])
 
         return InlineKeyboardMarkup(keyboard)
 
     def select_day_command(self, bot, update):
         self.logger.info('select days command')
-        user_id = update.effective_user.id
+        trainee_id = update.effective_trainee.id
         group_id = update.message.chat_id
 
-        user = self.users.get(user_id)
-        self.logger.info('user to select days %s', user)
-        group = self.groups.get(group_id)
+        trainee = Trainee.objects.get(id=trainee_id)
+        self.logger.info('trainee to select days %s', trainee)
+        group = Group.objects.get(id=group_id)
         self.logger.info('the group is %s', group)
 
         if not group:
             self.logger.info('the group does not exist in DB')
-            group = Group(id=group_id)
-            self.session.add(group)
+            group = Group.create(group_id=group_id)
             self.logger.info('created group %s', group)
 
-        if user is None:  # new user.
-            self.logger.info('user does not exist in the DB')
-            user = User(id=user_id, first_name=update.effective_user.first_name)
-            self.session.add(user)
-            self.logger.info('created user in DB %s', user)
-            group.users.append(user)
-            self.logger.info('added the user to group')
+        if trainee is None:  # new trainee.
+            self.logger.info('trainee does not exist in the DB')
+            trainee = Trainee.create(trainee_id=trainee_id, first_name=update.effective_trainee.first_name)
+            self.logger.info('created trainee in DB %s', trainee)
+            group.add_trainee(new_trainee=trainee)
+            self.logger.info('added the trainee to group')
 
-        elif user not in group.users:  # user exists but new in the current group.
-            self.logger.info('user was not in the group')
-            group.users.append(user)
+        elif trainee not in group.trainees:  # trainee exists but new in the current group.
+            self.logger.info('trainee was not in the group')
+            group.add_trainee(new_trainee=trainee)
 
-        keyboard = self._generate_inline_keyboard_for_select_days(user)
+        keyboard = self._generate_inline_keyboard_for_select_days(trainee)
         update.message.reply_text('באיזה ימים אתה מתאמן יא בוט?', reply_markup=keyboard)
 
     def select_day(self, bot, update):
         self.logger.info('select day')
         query = update.callback_query
 
-        user = self.users.get(update.effective_user.id)
-        self.logger.info('user selected %s', user)
-        _, user_id, selected_day_index = query.data.split()
+        trainee = Trainee.objects.get(id=update.effective_trainee.id)
+        self.logger.info('trainee selected %s', trainee)
+        _, trainee_id, selected_day_index = query.data.split()
 
-        if user is None or user.id != user_id:
-            self.logger.info('user is not allow to choose for others')
+        if trainee is None or trainee.id != unicode(trainee_id):
+            self.logger.info('trainee is not allow to choose for others')
             bot.answerCallbackQuery(text='אי אפשר לבחור לאחרים יא בוט',
                                     callback_query_id=update.callback_query.id,
                                     parse_mode=ParseMode.HTML)
             return
 
         logger.info('selected day index %s', selected_day_index)
-        selected_day = DAYS_NAME[int(selected_day_index)]
-        selected_day = self.days.get(selected_day)
+        selected_day = trainee.training_days[int(selected_day_index)]
         self.logger.info('selected day %s', selected_day)
-        if user not in selected_day.users:
-            self.logger.info('new selected day, adding it to the user training days')
-            selected_day.users.append(user)
+        if selected_day.selected:
+            self.logger.info('already selected day, removing it from the trainee training days')
         else:
-            self.logger.info('already selected day, removing it from the user training days')
-            selected_day.users.remove(user)
-        self.session.commit()
+            self.logger.info('new selected day, adding it to the trainee training days')
 
-        updated_keyboard = self._generate_inline_keyboard_for_select_days(user)
+        selected_day.selected = not selected_day.selected
+        trainee.save()
+
+        updated_keyboard = self._generate_inline_keyboard_for_select_days(trainee)
         bot.edit_message_reply_markup(chat_id=query.message.chat_id,
                                       message_id=query.message.message_id,
                                       reply_markup=updated_keyboard)
@@ -113,78 +106,76 @@ class GymBot(object):
 
     def my_days_command(self, bot, update):
         self.logger.info('my days command')
-        user = self.users.get(update.effective_user.id)
-        self.logger.info('asking user %s', user)
-        text = ' '.join(unicode(day) for day in user.days)
-        self.logger.info('user days %s', text)
+        trainee = Trainee.objects.get(id=update.effective_trainee.id)
+        self.logger.info('requested by trainee %s', trainee)
+        text = ' '.join(day.name for day in trainee.training_days.filter(selected=True))
+        self.logger.info('trainee days %s', text)
         bot.send_message(chat_id=update.message.chat_id,
                          text=text)
 
     def _groups_daily_timer(self, callback):
         self.logger.info('group daily reminder')
         self.logger.info('callback method: %s', callback.func_name)
-        for group in self.groups.all():
-            self.logger.info('checking group %s %s %s', group, 'with users', group.users)
+        for group in Group.objects.all():
+            self.logger.info('checking group %s', group)
             today_name = upper_first_letter(datetime.now().strftime('%A'))
-            today = self.days.get(today_name)
-            self.logger.info('the day is %s', today)
+            self.logger.info('the day is %s', today_name)
 
-            # TODO: use sql query.
-            relevant_users = [user for user in today.users if user in group.users]
-            self.logger.info('relevant users are %s', relevant_users)
+            relevant_trainees = group.get_trainees_in_day(today_name)
+            self.logger.info('relevant trainees are %s', relevant_trainees)
 
-            callback(group, relevant_users)
+            callback(group, relevant_trainees)
 
         threading.Timer(timedelta(hours=23, minutes=59, seconds=59).total_seconds(),
                         self._groups_daily_timer,
                         args=(callback, )).start()
         self.logger.info('set %s timer for tomorrow', callback.func_name)
 
-    def go_to_gym(self, group, relevant_users):
+    def go_to_gym(self, group, relevant_trainees):
         self.logger.info('reminding to go to the gym')
-        if not relevant_users:
-            self.logger.info('there are no relevant users')
+        if not relevant_trainees:
+            self.logger.info('there are no relevant trainees')
             return
         go_to_gym_plural = 'לכו היום לחדר כושר יא בוטים {training}'
         go_to_gym_individual = 'לך היום לחדר כושר יא בוט {training}'
 
-        training_today_msg = ' '.join('@' + user.first_name for user in relevant_users)
+        training_today_msg = ' '.join('@' + trainee.first_name for trainee in relevant_trainees)
 
-        if len(relevant_users) > 1:
-            self.logger.info('more than one user therefore creating plural msg')
+        if len(relevant_trainees) > 1:
+            self.logger.info('more than one trainee therefore creating plural msg')
             text = go_to_gym_plural.format(training=training_today_msg)
         else:
-            self.logger.info('one user creating msg for individual')
+            self.logger.info('one trainee creating msg for individual')
             text = go_to_gym_individual.format(training=training_today_msg)
 
         self.updater.bot.send_message(chat_id=group.id, text=text)
         self.logger.info('finished to remind to group %s', group)
 
-    def went_to_gym(self, group, relevant_users):
+    def went_to_gym(self, group, relevant_trainees):
         self.logger.info('asking who went to the gym')
-        self.logger.info('relevant users %s', relevant_users)
-        if not relevant_users:
-            self.logger.info('there are no relevant users')
+        self.logger.info('relevant trainees %s', relevant_trainees)
+        if not relevant_trainees:
+            self.logger.info('there are no relevant trainees')
             return
 
         went_to_gym_plural = 'הלכתם היום לחדכ יא בוטים? {training}'
         went_to_gym_individual = 'הלכת היום לחדכ יא בוט? {training}'
-        training_today_msg = ' '.join('@' + user.first_name for user in relevant_users)
+        training_today_msg = ' '.join('@' + trainee.first_name for trainee in relevant_trainees)
 
-        if len(relevant_users) > 1:
-            self.logger.info('more than one user therefore creating plural msg')
+        if len(relevant_trainees) > 1:
+            self.logger.info('more than one trainee therefore creating plural msg')
             text = went_to_gym_plural.format(training=training_today_msg)
         else:
-            self.logger.info('one user creating msg for individual')
+            self.logger.info('one trainee creating msg for individual')
             text = went_to_gym_individual.format(training=training_today_msg)
 
-        allowed_users = ','.join(unicode(user.id) for user in relevant_users)
+        allowed_trainees = ','.join(unicode(trainee.id) for trainee in relevant_trainees)
         keyboard = [[InlineKeyboardButton('כן',
-                                          callback_data='went_to_gym [{allowed_users}] yes'.format(
-                                                        allowed_users=allowed_users)),
+                                          callback_data='went_to_gym [{allowed_trainees}] yes'.format(
+                                              allowed_trainees=allowed_trainees)),
                      InlineKeyboardButton('אני אפס',
-                                          callback_data='went_to_gym [{allowed_users}] no'.format(
-                                                        allowed_users=allowed_users))]]
+                                          callback_data='went_to_gym [{allowed_trainees}] no'.format(
+                                              allowed_trainees=allowed_trainees))]]
 
         self.updater.bot.send_message(chat_id=group.id,
                                       text=text,
@@ -194,28 +185,28 @@ class GymBot(object):
     def went_to_gym_answer(self, bot, update):
         self.logger.info('answer to went to gym question')
         query = update.callback_query
-        _, allowed_users, answer = query.data.split()
-        allowed_users = json.loads(allowed_users)
-        self.logger.info('allowed to answer the question users %s', allowed_users)
-        user = self.users.get(update.effective_user.id)
-        self.logger.info('the user that answered %s', user)
+        _, allowed_trainees, answer = query.data.split()
+        allowed_trainees = json.loads(allowed_trainees)
+        self.logger.info('allowed to answer the question trainees %s', allowed_trainees)
+        trainee = Trainee.objects.get(id=update.effective_trainee.id)
+        self.logger.info('the trainee that answered %s', trainee)
 
-        if user is None or user.id not in allowed_users:
-            self.logger.info('the user is not allowed to answer the question')
+        if trainee is None or trainee.id not in allowed_trainees:
+            self.logger.info('the trainee is not allowed to answer the question')
             bot.answerCallbackQuery(text='זה לא היום שלך להתאמן יא בוט',
                                     callback_query_id=update.callback_query.id)
             return
 
         if answer == 'yes':
-            self.logger.info('%s %s', user.first_name, 'answered yes')
+            self.logger.info('%s %s', trainee.first_name, 'answered yes')
             bot.send_message(chat_id=query.message.chat_id,
-                             text='כל הכבוד {user} אלוף!'.format(user=user.first_name))
+                             text='כל הכבוד {trainee} אלוף!'.format(trainee=trainee.first_name))
             bot.answerCallbackQuery(text=THUMBS_UP_EMOJI,
                                     callback_query_id=update.callback_query.id)
         else:
-            self.logger.info('%s %s', user.first_name, 'answered no')
+            self.logger.info('%s %s', trainee.first_name, 'answered no')
             bot.send_message(chat_id=query.message.chat_id,
-                             text='אפס מאופס {user}'.format(user=user.first_name))
+                             text='אפס מאופס {trainee}'.format(trainee=trainee.first_name))
             bot.answerCallbackQuery(text=THUMBS_DOWN_EMOJI,
                                     callback_query_id=update.callback_query.id)
 
@@ -271,11 +262,10 @@ class GymBot(object):
         self.logger.info('new group id is %s', group_id)
         self.logger.info('new chat member in the group is %s', new_chat_member.first_name)
 
-        if new_chat_member.id == self.updater.bot.id and self.groups.get(group_id) is None:  # bot joined to new group
+        if new_chat_member.id == self.updater.bot.id and Group.objects.get(id=group_id) is None:  # bot joined to new group
             self.logger.info('bot joined to new group')
-            group = Group(id=update.message.chat_id)
-            self.session.add(group)
-            self.logger.info('created instance of the new group in the DB')
+            group = Group.create(group_id=update.message.chat_id)
+            self.logger.info('created instance of the new group in the DB %s', group)
 
     def run(self):
         self.logger.info('starting to run')
@@ -299,15 +289,17 @@ if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         os.environ['BOT_TOKEN'] = os.environ['BOT_TOKEN_TEST']
-        os.environ['POSTGRES_URL_CON'] = os.environ['POSTGRES_URL_CON_TEST']
+        os.environ['MONGODB_URL_CON'] = os.environ['MONGODB_URL_CON_TEST']
 
     token = os.environ['BOT_TOKEN']
-    db_con_string = os.environ['POSTGRES_URL_CON']
+    db_con_string = os.environ['MONGODB_URL_CON']
+
+    from mongoengine import connect
+    connect(host=os.environ['MONGODB_URL_CON'])
 
     updater = Updater(token=token)
     dispatcher = updater.dispatcher
     logger = logging.getLogger(__name__)
     logger.addHandler(logging.StreamHandler())
 
-    db_session = get_db_session(db_con_string)
-    GymBot(db_session, updater, dispatcher, logger).run()
+    GymBot(updater, dispatcher, logger).run()
--- models.py
+++ models.py
@@ -1,15 +1,100 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+from utils import DAYS_NAME, upper_first_letter
 from mongoengine import (Document,
+                         ListField,
                          StringField,
                          BooleanField,
-                         )
+                         EmbeddedDocument,
+                         EmbeddedDocumentListField,
+                         CachedReferenceField,
+                         QuerySet,
+                         DoesNotExist)
 
 
-class Day(Document):
+class ExtendedQuerySet(QuerySet):
+    def get(self, *q_objs, **query):
+        try:
+            if 'id' in query:
+                query['id'] = unicode(query['id'])
+            return super(ExtendedQuerySet, self).get(*q_objs, **query)
+        except DoesNotExist:
+            return None
+
+
+class Day(EmbeddedDocument):
     name = StringField(required=True, max_length=64)
     selected = BooleanField(default=False)
 
+    @classmethod
+    def get_week_days(cls):
+        return [cls(day_name) for day_name in DAYS_NAME]
+
+    def __repr__(self):
+        return '<Day {day_name} {selected}>'.format(day_name=self.name,
+                                                    selected='selected' if self.selected
+                                                             else 'not selected')
+
+    def __str__(self):
+        return repr(self)
+
+    def __unicode__(self):
+        return repr(self)
+
 
 class Trainee(Document):
-    id = StringField(required=True, primary_key=True, unique=True)
+    id = StringField(required=True, primary_key=True)
     first_name = StringField(required=True)
-    training_days =
+    training_days = EmbeddedDocumentListField(Day)
+
+    meta = {'queryset_class': ExtendedQuerySet}
+
+    # TODO: think if to add it under query manager.
+    @classmethod
+    def create(cls, trainee_id, first_name):
+        new_trainee = cls(id=unicode(trainee_id), first_name=first_name)
+        new_trainee.training_days.extend(Day.get_week_days())
+
+        return new_trainee.save()
+
+    def __repr__(self):
+        return '<Trainee {id} {first_name}>'.format(id=self.id,
+                                                    first_name=self.first_name)
+
+    def __str__(self):
+        return repr(self)
+
+    def __unicode__(self):
+        return repr(self)
+
+
+class Group(Document):
+    id = StringField(required=True, primary_key=True)
+    trainees = ListField(CachedReferenceField(Trainee, auto_sync=True))
+
+    meta = {'queryset_class': ExtendedQuerySet}
+
+    @classmethod
+    def create(cls, group_id, users=[]):
+        new_group = cls(id=unicode(group_id))
+        new_group.trainees.extend(users)
+
+        return new_group.save()
+
+    def add_trainee(self, new_trainee):
+        self.trainees.append(new_trainee)
+        return self.save()
+
+    def get_trainees_in_day(self, day_name):
+        day_idx = DAYS_NAME.index(upper_first_letter(day_name))
+        return [trainee for trainee in self.trainees if trainee.training_days[day_idx].selected]
+
+    def __repr__(self):
+        return '<Group {id} [{trainees}]>'.format(id=self.id,
+                                                  trainees='{trainee},'.join(str(trainee) for trainee in self.trainees))
+
+    def __str__(self):
+        return repr(self)
+
+    def __unicode__(self):
+        return repr(self)
