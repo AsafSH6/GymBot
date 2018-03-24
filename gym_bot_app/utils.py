e89709d8765025aaf763b9136fa2980c2cc2e310
Asaf <AsafsEmail@gmail.com>
2018-03-24 13:26:59 +0300

    using emoji lib instead of raw unicode.

--- gym_bot_app/utils.py
+++ gym_bot_app/utils.py
@@ -1,10 +1,10 @@
 # encoding: utf-8
 from __future__ import unicode_literals
+import emoji
 
-
-WEIGHT_LIFTER_EMOJI = u'🏋️'
-THUMBS_UP_EMOJI = u'👍'
-THUMBS_DOWN_EMOJI = u'👎'
+WEIGHT_LIFTER_EMOJI = emoji.emojize(':person_lifting_weights:')
+THUMBS_UP_EMOJI = emoji.emojize(':thumbs_up:')
+THUMBS_DOWN_EMOJI = emoji.emojize(':thumbs_down:')
 
 DAYS_NAME = 'Sunday Monday Tuesday Wednesday Thursday Friday Saturday'.split()
 HEBREW_DAYS_NAME = 'ראשון שני שלישי רביעי חמישי שישי שבת'.split()
