6d8c2813ab1737d85fe3dd5ce4355b7643604558
ddomb <domb200@gmail.com>
2020-10-11 22:37:29 +0300

    Removed the Quote class and fixed asaf's requests.

--- gym_bot_app/commands/motivation_quotes.py
+++ gym_bot_app/commands/motivation_quotes.py
@@ -13,40 +13,22 @@ class MotivationQuotesCommand(Command):
 
     """
     
-    class Quote():
-        
-        def __init__(self, text, auther):
-            self.text = text
-            self.auther = auther
-            self.quote_msg = textwrap.dedent('"{text}", {auther}')
-
-        @property
-        def text(self, text):
-            return text
-        
-        @property
-        def auther(self,auther):
-            return auther
-        
-        def compose_quote(self):
-            return self.quote_msg.format(
-                                    text=self.text,
-                                    auther=self.auther
-                                    )
-
     DEFAULT_COMMAND_NAME = 'motivate_me'
+
+    QUOTE_MSG = textwrap.dedent('"{text}", {auther}')
     
     QUOTE_LIST = [
-        Quote('The last three or four reps is what makes the muscle grow. This area of pain divides a champion from someone who is not a champion.', 'Arnold Schwarzenegger'),
-        Quote('Success usually comes to those who are too busy to be looking for it.','Henry David Thoreau'),
-        Quote('All progress takes place outside the comfort zone','Michael John Bobak'),
-        Quote('If you think lifting is dangerous, try being weak. Being weak is dangerous.','Bret Contreras'),
-        Quote('The only place where success comes before work is in the dictionary.','Vidal Sassoon'),
-        Quote('The clock is ticking. Are you becoming the person you want to be?','Greg Plitt'),
-        Quote('Whether you think you can, or you think you can’t, you’re right.','Henry Ford,'),
-        Quote('The successful warrior is the average man, with laser-like focus.','Bruce Lee'),
-        Quote('You must expect great things of yourself before you can do them.','Michael Jordan'),
-        Quote('Action is the foundational key to all success.','Pablo Picasso')
+        {'text': 'The last three or four reps is what makes the muscle grow. This area of pain divides a champion from someone who is not a champion.', 'auther': 'Arnold Schwarzenegger'},
+        {'text': 'Success usually comes to those who are too busy to be looking for it.', 'auther': 'Henry David Thoreau'},
+        {'text': 'All progress takes place outside the comfort zone', 'auther': 'Michael John Bobak'},
+        {'text': 'If you think lifting is dangerous, try being weak. Being weak is dangerous.', 'auther': 'Bret Contreras'},
+        {'text': 'The only place where success comes before work is in the dictionary.', 'auther': 'Vidal Sassoon'},
+        {'text': 'The clock is ticking. Are you becoming the person you want to be?', 'auther': 'Greg Plitt'},
+        {'text': 'Whether you think you can, or you think you can’t, you’re right.', 'auther': 'Henry Ford'},
+        {'text': 'The successful warrior is the average man, with laser-like focus.', 'auther': 'Bruce Lee'},
+        {'text': 'You must expect great things of yourself before you can do them.', 'auther': 'Michael Jordan'},
+        {'text': 'Action is the foundational key to all success.', 'auther': 'Pablo Picasso'},
+        {'text': 'Everyone is a bot', 'auther': 'Asaf sh'}
         ]
     
 
@@ -67,5 +49,12 @@ class MotivationQuotesCommand(Command):
         random_quote = self.QUOTE_LIST[random_number]
 
         update.message.reply_text(quote=True,
-                                  text=random_quote.compose_quote())
+                                  text=self.compose_quote(random_quote))
+    
+    def compose_quote(self, quote_object):
+        return self.QUOTE_MSG.format(
+                                text=quote_object.text,
+                                auther=quote_object.auther
+                                )
+
 
