2fee566ba0ab41c09e0e77a05c1d6e20d4c3da12
rw_master <rw_master@master.com>
2021-10-13 00:41:47 +0300

    Don't allow user to select day to train that has already passed

--- gym_bot_app/commands/select_days.py
+++ gym_bot_app/commands/select_days.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from telegram import ParseMode, error
 from telegram.ext import CallbackQueryHandler
 
-from gym_bot_app import LYING_FACE_EMOJI
+from gym_bot_app import DAYS_NAME, LYING_FACE_EMOJI
 from gym_bot_app.commands import Command
 from gym_bot_app.keyboards import trainee_select_days_inline_keyboard
 from gym_bot_app.decorators import get_trainee_and_group
@@ -22,6 +22,7 @@ class SelectDaysCommand(Command):
 
     SELECT_DAYS_MSG = 'באיזה ימים אתה מתאמן יא בוט?'
     CANT_CHOOSE_TO_OTHERS_MSG = 'אי אפשר לבחור לאחרים יא בוט'
+    CANT_CHOOSE_BEFORE_CURRENT_DAY = 'אי אפשר לבחור יום שכבר חלף יא בוט'
     ALREADY_CHANGED_IN_ANOTHER_PLACE_MSG = 'יא בוט על חלל, כבר שינית את זה במקום אחר...'
     CANCELED_TRAINING_DAY_ON_THE_SAME_DAY = 'אני אולי בוט אבל אני לא אדיוט! {{trainee_name}} ביטל את האימון שלו היום {emoji}'.format(emoji=LYING_FACE_EMOJI)
 
@@ -82,6 +83,14 @@ class SelectDaysCommand(Command):
         try:
             selected_day = trainee.training_days.get(name=selected_day)
             self.logger.debug('Selected day %s', selected_day)
+            current_day_index = (datetime.today().weekday() + 1) % len(DAYS_NAME)
+            selected_day_index = DAYS_NAME.index(selected_day.name)
+            if selected_day_index < current_day_index:
+                self.logger.debug('Trainee`s selected day index %s is before current day index %s', selected_day_index, current_day_index)
+                bot.answerCallbackQuery(text=self.CANT_CHOOSE_BEFORE_CURRENT_DAY,
+                                    callback_query_id=update.callback_query.id,
+                                    parse_mode=ParseMode.HTML)
+                return
             selected_day.selected = not selected_day.selected
             updated_keyboard = self.get_select_days_keyboard(trainee=trainee)
             bot.edit_message_reply_markup(chat_id=query.message.chat_id,
